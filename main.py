112b5e23adbcebcba0343a4419cdb4404679d3c1
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-11 12:08:00 +0100

    main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from Clases.ejercicio4 import ejercicio4
+from Clases.ejercicio5 import ejercicio5
 
 if __name__=='__main__':
     print('Ejercicio 4:\n')
     ejercicio4()
     print('Ejercicio 5:\n')
-    
+    ejercicio5()
     print('Ejercicio 6:\n')
-    
+    ejercicio6()
