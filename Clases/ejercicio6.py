a6d29ff328f0fa30cbeac51610b00de6b62ffa89
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-12 13:25:32 +0100

    ejercicio 6 hecho

--- Clases/ejercicio6.py
+++ Clases/ejercicio6.py
@@ -1,2 +1,28 @@
 def ejercicio6():
-    print()
+    
+    t=[4,3,8,9,7,6,5,2,1]
+    print("Tabla inicial:")
+    print(t)
+
+    max=t[0]
+    ini=0
+    cont=1
+    h=int(len(t))
+
+    for i in range(1,h):
+        if(t[i]<=max):
+            t[i-1]=t[i]
+        else:
+            t[i-1]=max
+            max=t[i]
+            print("Segmento "+str(cont)+":")
+            print(t[ini:i])
+            ini=i
+            cont=cont+1
+
+    t[h-1]=max
+    print("Segmento "+str(cont)+":")
+    print(t[ini:h])
+
+    print("Tabla ordenada:")
+    print(t)
