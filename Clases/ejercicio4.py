b59099fffb74c778302b5abf7a5dc3b89d4bce3d
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-12 12:58:01 +0100

    ejercicio 4 hecho

--- Clases/ejercicio4.py
+++ Clases/ejercicio4.py
@@ -1,16 +1,20 @@
 def ejercicio4():    
     #VECTOR
+
     t=[3,4,8,9,7,6,5,2,1]
+    print("Tabla inicial:")
+    print(t)
 
     tmp=0
 
     h=int(len(t))
 
-    for i in range(0,h-1):
+    for i in range(1,h):
         tmp=t[i]
         j=i-1
-        while(j>0 and t[j]>=tmp):
+        while(j>=0 and t[j]>=tmp):
            t[j+1]=t[j]
            j=j-1
-        t[j]=tmp
+        t[j+1]=tmp
+    print("Tabla ordenada:")
     print(t)
